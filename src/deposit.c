9433a79eda5df26b71e407abb32a5819075d8882
Винник Анна Евгеньевна <vinnik.anna@rambler.ru>
2017-05-15 08:38:45 +0700

    Changed deposit.c

--- src/deposit.c
+++ src/deposit.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "deposit.h"
 
-float print_income(int time, int sum){
+float print_income(int time, int summ){
     if (time > (-1) && time < 31)
-        return sum*0.1*(-1);
+        return summ*0.1*(-1);
     else if (time > 30 && time < 121)
-        return sum*0.02;
+        return summ*0.02;
     else if (time > 120 && time < 241)
-        return sum*0.06;
+        return summ*0.06;
     else if (time > 240 && time < 366)
-        return sum*0.12;
+        return summ*0.12;
     else 
         return 0;      
 }
